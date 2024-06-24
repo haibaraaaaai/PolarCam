797881a6bb797603cdd2bf2744e1a22ac7806d5e
haibaraaaaai <dennyxudp0402@gmail.com>
2024-06-24 21:36:47 +0100

    Need to fix main module, or revert back to unpackaged state

--- legacy/main.py
+++ legacy/main.py
@@ -1,17 +1,20 @@
 from PySide6.QtWidgets import QApplication
-from polar_cam import CameraControl, ImageProcessor, DataAnalyzer, MainWindow
+from polar_cam.camera_control import CameraControl
+from polar_cam.image_processor import ImageProcessor
+from polar_cam.data_analyzer import DataAnalyzer
+from polar_cam.main_window import MainWindow
 import sys
 
 def main():
     app = QApplication(sys.argv)
-   
+
     camera_control = CameraControl()
     image_processor = ImageProcessor()
     data_analyzer = DataAnalyzer()
 
     main_window = MainWindow(camera_control, image_processor, data_analyzer)
     main_window.show()
-    
+
     camera_control.initialize_and_start_acquisition()
 
     sys.exit(app.exec())
